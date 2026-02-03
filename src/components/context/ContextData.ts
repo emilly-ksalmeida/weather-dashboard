84266e25319e06697f876249bf73ada56849ca6a
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-02-03 18:50:59 -0300

    refactor(react-architecture): centralize city geolocation state in context
    - No contexto, é mantido apenas o estado responsável pelos dados de geolocalização necessários para a obtenção da previsão do clima.
    - Essa centralização foi necessária, pois a geolocalização é o único dado que pode ter duas origens: pesquisa via formulário ou clique no mapa.
    - Ambos os componentes de entrada conseguem atualizar diretamente o estado de coordinates.

--- src/components/context/ContextData.ts
+++ src/components/context/ContextData.ts
@@ -1,12 +1,9 @@
 import { createContext } from "react";
-import type { Location, Geocoding } from "@/utils/types";
+import type { Geocoding } from "@/utils/types";
 
 interface IProviderContextProps {
-  location: Location;
-  setLocation: React.Dispatch<React.SetStateAction<Location>>;
   coordinates: Geocoding;
   setCoordinates: React.Dispatch<React.SetStateAction<Geocoding>>;
-  onMapClick: (latitude: number, longitude: number) => void;
 }
 
 const ContextData = createContext<IProviderContextProps>({} as IProviderContextProps);
